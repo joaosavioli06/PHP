970559e8887b8da6bdd66b381bb3030d8d5f4475
João Gabriel Savioli <108081618+joaosavioli06@users.noreply.github.com>
2023-05-05 18:13:14 -0300

    Arquivo criado

---
+++ variaveis.php
@@ -0,0 +1,20 @@
+<?php
+//Atribuindo o nome "João Gabriel" que é do tipo STRING à variável $nome.
+$nome = "João Gabriel";
+//Atribuindo o valor 17 que é do tipo INTEGER à variável $idade.
+$idade = 17;
+//Atribuindo o valor 1.76 que é do tipo DOUBLE à varável $altura.
+$altura = 1.76;
+// Atribuindo o valor true que é do tipo BOOLEAN à variável $brasileiro
+$brasileiro = true;
+
+echo "Olá, Mundo!\n";
+echo "Olá, meu nome é $nome.\nTenho $idade anos! Meço $altura.\nSou ";
+echo $brasileiro ? "brasileiro" : "estrangeiro"; 
+echo "\n";
+// echo $nome;
+// echo $idade;
+// echo $altura;
+// echo $brasileiro;
+// Se você quiser escrever como se fosse um texto, use aspas simples '. Se quiser escrever chamando variáveis, quebrando linha e etc, usa aspas dupals ".
+?>
